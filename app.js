8ad4b3f4d837eff0af128e35a2615fb5b6b5755c
keinishikawa <keinishikawa4@gmail.com>
2021-03-12 18:37:11 +0900

    prettier installed, editing /browse/home

--- app.js
+++ app.js
@@ -1,54 +1,25 @@
 // ====================事前準備処理=======================
 // ライブラリインポート
-const express = require('express');
-const mysql = require('mysql');
-const session = require('express-session');
-const bcrypt = require('bcrypt');
+const express = require("express");
+const mysql = require("mysql");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
 const app = express();
 
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: false}));
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
 // ポート3000番でローカルホストサーバ起動
 app.listen(3000);
 
-// ====================共通処理=======================
-
-// リクエスト時に毎回処理する内容
-  // セッション確立
-app.use(
-    session({
-      secret: 'my_secret_key',
-      resave: false,
-      saveUninitialized: false,
-    })
-  );
-
-  //ログイン中にユーザ名を表示する処理
-  app.use((req, res, next) => {
-    if (req.session.userId === undefined) {
-      res.locals.username = 'ゲスト';
-      res.locals.isLoggedIn = false;
-    } else {
-      res.locals.username = req.session.username;
-      res.locals.email = req.session.email;
-      res.locals.isLoggedIn = true;
-      if(req.session.status===1){
-        res.locals.status = "暇や";  
-      }else{
-        res.locals.status = "暇やない";
-      }
-    }
-    next();
-  });
 
 // ====================各ルーティング処理=======================
 
 //　ユーザ一般とCP管理者どっちの画面か選ぶ画面の表示
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
   res.render("./components/select-page-type.ejs");
-})
+});
 
-app.use("/browse",require("./routes/browse.js")());
-app.use("/admin",require("./routes/admin.js")());
-app.use("/cs",require("./routes/cs.js")());
+app.use("/browse", require("./routes/browse.js")());
+app.use("/admin", require("./routes/admin.js")());
+app.use("/cs", require("./routes/cs.js")());
--- routes/cs.js
+++ routes/cs.js
@@ -1,28 +1,26 @@
 // ====================事前準備処理=======================
 // ライブラリインポート
-const express = require('express');
-const mysql = require('mysql');
-const session = require('express-session');
-const bcrypt = require('bcrypt');
-
+const express = require("express");
+const mysql = require("mysql");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
 
 // DBへの接続
 const connection = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"Pandaman@2600",
-    database:"NextPD"
+  host: "localhost",
+  user: "root",
+  password: "Pandaman@2600",
+  database: "NextPD"
 });
 
 // DBに接続時のエラー処理
-connection.connect((err)=>{
-    if(err){
-        console.log("error connecting:"+err.stack);
-        return;
-    }
-    console.log("success");
-})
-
+connection.connect(err => {
+  if (err) {
+    console.log("error connecting:" + err.stack);
+    return;
+  }
+  console.log("success");
+});
 
 // ************cs******************
 
@@ -30,149 +28,139 @@ connection.connect((err)=>{
 
 module.exports = () => {
   const router = express.Router();
- // ログイン画面の表示
-  router.get('/', (req, res) => {
-    res.render('./components/cs/login.ejs');
+  // ログイン画面の表示
+  router.get("/", (req, res) => {
+    res.render("./components/cs/login.ejs");
   });
 
-    // ログインリクエストをポストしたときの処理
-    router.post('/', (req, res) => {
+  // ログインリクエストをポストしたときの処理
+  router.post("/", (req, res) => {
     const email = req.body.email;
     connection.query(
-        'SELECT * FROM csusers WHERE email = ?',
-        [email],
-        (error, results) => {
+      "SELECT * FROM csusers WHERE email = ?",
+      [email],
+      (error, results) => {
         if (results.length > 0) {
-            const plain = req.body.password;
-            const hash = results[0].password;
+          const plain = req.body.password;
+          const hash = results[0].password;
 
-            bcrypt.compare(plain, hash,(error, isEqual) => {
-            if(isEqual){
-                req.session.userId = results[0].id;
-                req.session.username = results[0].username;
-                req.session.email = results[0].email;
-                req.session.status = results[0].status;
-                res.redirect(req.baseUrl +'/home');
+          bcrypt.compare(plain, hash, (error, isEqual) => {
+            if (isEqual) {
+              req.session.userId = results[0].id;
+              req.session.username = results[0].username;
+              req.session.email = results[0].email;
+              req.session.status = results[0].status;
+              res.redirect(req.baseUrl + "/home");
             } else {
-                console.log("false");
-                res.redirect(req.baseUrl);
+              console.log("false");
+              res.redirect(req.baseUrl);
             }
-            });
-        
+          });
         } else {
-            res.redirect(req.baseUrl);
+          res.redirect(req.baseUrl);
 
-            console.log("false2");
+          console.log("false2");
         }
-        }
+      }
     );
-    });
+  });
 
+  // パスワードを忘れたら画面の表示
+  router.get("/forgetpassword", (req, res) => {
+    res.render("./components/cs/forgetpassword.ejs");
+  });
 
+  // 新規登録画面の表示
+  router.get("/signup", (req, res) => {
+    res.render("./components/cs/signup.ejs", { errors: [] });
+  });
 
-    // パスワードを忘れたら画面の表示
-    router.get("/forgetpassword",(req,res)=>{
-    res.render('./components/cs/forgetpassword.ejs');
-    });
-
-    // 新規登録画面の表示
-    router.get('/signup', (req, res) => {
-    res.render('./components/cs/signup.ejs', { errors: [] });
-    });
-
-    // ポスト時の処理
-    router.post('/signup', 
+  // ポスト時の処理
+  router.post(
+    "/signup",
     // 入力値の空チェック
     (req, res, next) => {
-        console.log('入力値の空チェック');
-        const username = req.body.username;
-        const email = req.body.email;
-        const password = req.body.password;
-        const errors = [];
+      console.log("入力値の空チェック");
+      const username = req.body.username;
+      const email = req.body.email;
+      const password = req.body.password;
+      const errors = [];
 
-        if (username === '') {
-        errors.push('ユーザー名が空です');
-        }
+      if (username === "") {
+        errors.push("ユーザー名が空です");
+      }
 
-        if (email === '') {
-        errors.push('メールアドレスが空です');
-        }
+      if (email === "") {
+        errors.push("メールアドレスが空です");
+      }
 
-        if (password === '') {
-        errors.push('パスワードが空です');
-        }
+      if (password === "") {
+        errors.push("パスワードが空です");
+      }
 
-        if (errors.length > 0) {
-        res.render('./components/cs/signup.ejs', { errors: errors });
-        } else {
+      if (errors.length > 0) {
+        res.render("./components/cs/signup.ejs", { errors: errors });
+      } else {
         next();
-        }
+      }
     },
     // すでに登録してあるかチェック
     (req, res, next) => {
-        console.log('メールアドレスの重複チェック');
-        const email = req.body.email;
-        const errors = [];
-        connection.query(
-        'SELECT * FROM csusers WHERE email = ?',
+      console.log("メールアドレスの重複チェック");
+      const email = req.body.email;
+      const errors = [];
+      connection.query(
+        "SELECT * FROM csusers WHERE email = ?",
         [email],
         (error, results) => {
-            if (results.length > 0) {
-            errors.push('ユーザー登録に失敗しました');
-            res.render('./components/cs/signup.ejs', { errors: errors });
-            } else {
+          if (results.length > 0) {
+            errors.push("ユーザー登録に失敗しました");
+            res.render("./components/cs/signup.ejs", { errors: errors });
+          } else {
             next();
-            }
+          }
         }
-        );
+      );
     },
     // ユーザー登録処理
     (req, res) => {
-        console.log('ユーザー登録');
-        const username = req.body.username;
-        const email = req.body.email;
-        const password = req.body.password;
-        const status = 0;
-        bcrypt.hash(password, 10, (error, hash) => {
+      console.log("ユーザー登録");
+      const username = req.body.username;
+      const email = req.body.email;
+      const password = req.body.password;
+      const status = 0;
+      bcrypt.hash(password, 10, (error, hash) => {
         connection.query(
-            'INSERT INTO csusers (username, email, password, status) VALUES (?, ?, ?, ?)',
-            [username, email, hash, status],
-            (error, results) => {
+          "INSERT INTO csusers (username, email, password, status) VALUES (?, ?, ?, ?)",
+          [username, email, hash, status],
+          (error, results) => {
             req.session.userId = results.insertId;
             req.session.username = username;
             req.session.email = email;
-            res.redirect(req.baseUrl +'/home');
-            }
+            res.redirect(req.baseUrl + "/home");
+          }
         );
-        });
+      });
     }
-    );
+  );
 
-
-
-    //ログアウト時の処理
-    router.get('/logout', (req, res) => {
+  //ログアウト時の処理
+  router.get("/logout", (req, res) => {
     const email = req.session.email;
-    connection.query(
-        "UPDATE csusers SET status=0 WHERE email = ?",
-        [email]
-        );
+    connection.query("UPDATE csusers SET status=0 WHERE email = ?", [email]);
     req.session.destroy(error => {
-        res.redirect(req.baseUrl);
+      res.redirect(req.baseUrl);
     });
-    });
+  });
 
+  // ============トップ画面以降の処理=======================
+  //ログイン情報に応じたトップ画面の表示処理
+  router.get("/home", (req, res) => {
+    res.render("./components/cs/home.ejs");
+  });
 
-
-    // ============トップ画面以降の処理=======================
-    //ログイン情報に応じたトップ画面の表示処理
-    router.get('/home', (req, res) => {
-    res.render('./components/cs/home.ejs');
-    });
-
-
-    // コーポレート登録処理
-    router.post("/registerCorp",(req, res) => {
+  // コーポレート登録処理
+  router.post("/registerCorp", (req, res) => {
     const corporateType = req.body.corporateType;
     const corporateId = req.body.corporateId;
     const corporateName = req.body.corporateName;
@@ -181,20 +169,19 @@ module.exports = () => {
     const password = req.body.password;
     const status = 0;
     bcrypt.hash(password, 10, (error, hash) => {
-        connection.query(
-        'INSERT INTO users (username, email, password, status, corporateId) VALUES (?, ?, ?, ?, ?)',
+      connection.query(
+        "INSERT INTO users (username, email, password, status, corporateId) VALUES (?, ?, ?, ?, ?)",
         [username, email, hash, status, corporateId]
-        )
-        connection.query(
-        'INSERT INTO corporate (corporateType, corporateId, corporateName, corporateAdmin) VALUES (?, ?, ?, ?)',
+      );
+      connection.query(
+        "INSERT INTO corporate (corporateType, corporateId, corporateName, corporateAdmin) VALUES (?, ?, ?, ?)",
         [corporateType, corporateId, corporateName, email],
-        (error, results)=>{
-            res.redirect(req.baseUrl +"/home") ;
+        (error, results) => {
+          res.redirect(req.baseUrl + "/home");
         }
-        );
+      );
     });
-    }
-    );
+  });
 
-    return router;
-}
+  return router;
+};
